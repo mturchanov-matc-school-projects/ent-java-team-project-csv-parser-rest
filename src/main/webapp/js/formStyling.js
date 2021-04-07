23979c84769ca8d832b15fd6b1d0d301007c5e10
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-04-07 12:08:21 -0500

    Add functions to handle variable fields for querying parsed JSON

--- src/main/webapp/js/formStyling.js
+++ src/main/webapp/js/formStyling.js
@@ -5,9 +5,37 @@ const fileInputChanged = event => {
     label.textContent = fileName;
 }
 
+const hideAllVariableFields = () => {
+    let queryColumnGroup = document.querySelector("#queryColumnGroup");
+    queryColumnGroup.hidden = true;
+}
+
+const updateHiddenFields = queryType => {
+    let queryColumnGroup = document.querySelector("#queryColumnGroup");
+    hideAllVariableFields();
+    switch (queryType) {
+        case "value":
+            queryColumnGroup.hidden = false;
+            break;
+        case "all":
+        default:
+            break;
+    }
+}
+
+const queryTypeChanged = event => {
+    updateHiddenFields(event.target.value);
+}
+
 const formStyleInit = () => {
     let fileInputElement = document.querySelector("#fileInput");
     if (fileInputElement) fileInputElement.addEventListener("change", fileInputChanged);
+
+    let querySelectElement = document.querySelector("#queryTypeInput");
+    if (querySelectElement) {
+        querySelectElement.addEventListener("change", queryTypeChanged);
+        updateHiddenFields(querySelectElement.value);
+    }
 }
 
 window.addEventListener("load", formStyleInit);
