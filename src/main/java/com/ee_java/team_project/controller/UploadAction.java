33044e4a48791506b0764e8904d659aa22963422
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-04-11 16:59:24 -0500

    Add doPost to utilize external CSVFileWriter utility

--- src/main/java/com/ee_java/team_project/controller/UploadAction.java
+++ src/main/java/com/ee_java/team_project/controller/UploadAction.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import java.util.Properties;
 
 import com.ee_java.team_project.csv_parser.CodingCompCsvUtil;
+import com.ee_java.team_project.util.CSVFileWriter;
 import com.ee_java.team_project.util.PropertiesLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -33,24 +34,8 @@ import org.apache.logging.log4j.Logger;
         maxRequestSize = 1024 * 1024 * 100   	// 100 MB
 )
 public class UploadAction extends HttpServlet implements PropertiesLoader {
-    private Properties properties;
-    private String uploadPath;
 
     private final Logger logger = LogManager.getLogger();
-
-    public static final String PROPERTIES_PATH = "/webapp.properties";
-    public static final String UPLOAD_PATH_PROPERTY = "upload.path";
-
-    /**
-     * Initializes the servlet with a properties instance to load
-     * data from.
-     * @throws ServletException If the Servlet encounters an issue.
-     */
-    public void init() throws ServletException {
-        properties = loadProperties(PROPERTIES_PATH);
-        uploadPath = properties.getProperty(UPLOAD_PATH_PROPERTY);
-        initializeUploadPath(uploadPath);
-    }
 
     /**
      * Handles GET requests by redirecting to index page.
@@ -59,7 +44,7 @@ public class UploadAction extends HttpServlet implements PropertiesLoader {
      * @throws ServletException Whether or not the servlet encounters an error.
      * @throws IOException Whether or not an IO exception occurs.
      */
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.sendRedirect("index.jsp");
     }
 
@@ -73,29 +58,16 @@ public class UploadAction extends HttpServlet implements PropertiesLoader {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String url = "/index.jsp";
         String feedback = "Failed to upload the file. Please try again later.";
-        String path = writeUploadedFiles(request.getParts());
+        boolean success = processCSVFile(request);
 
-        // Verify that a file was successfully written in the upload directory
-        if (path != null) {
-            CodingCompCsvUtil parser = new CodingCompCsvUtil();
-            Map<List<String>, String> values = parser.readCsvFileFileWithoutPojo(path);
-
-            // Retrieves the raw JSON text from the values map, expecting only 1 entry in the map
-            Map.Entry<List<String>, String> entry = values.entrySet().iterator().next();
-            List<String> columns = entry.getKey();
-            String rawJson = entry.getValue();
-
-            HttpSession session = request.getSession();
-            session.setAttribute("columns", columns);
-            session.setAttribute("json", rawJson);
-
+        // Verify that CSV file was processed successfully
+        if (success) {
             url = "/endpoints.jsp";
-            feedback = "Successfully uploaded CSV file";
-
-            boolean success = deleteFile(path);
-            if (!success) {
-                logger.error("Unable to remove file {} for cleanup.", path);
+            String fileName = request.getParameter("fileName");
+            if (fileName != null) {
+                feedback = "Successfully parsed " + fileName + " to JSON.";
             }
+            feedback = "Successfully parsed CSV code.";
         }
 
         request.setAttribute("feedback", feedback);
@@ -105,60 +77,44 @@ public class UploadAction extends HttpServlet implements PropertiesLoader {
     }
 
     /**
-     * Creates the file path if it does not already exist.
-     * @param path The String path of the directory to create.
+     * Processes parsing of the uploaded file parts and stores it into the json session attribute.
+     * @param request The request made to the servlet for processing.
+     * @return Whether or not the processing succeeded without error.
      */
-    public void initializeUploadPath(String path) {
-        File filePath = new File(path);
-        if (!filePath.exists()) {
-            filePath.mkdirs();
-        }
-    }
+    public boolean processCSVFile(HttpServletRequest request) {
+        try {
+            Collection<Part> parts = request.getParts();
+            CSVFileWriter writer = new CSVFileWriter();
+            for (Part part : parts) {
+                String path = writer.write(part.getInputStream());
 
-    /**
-     * Writes the provided uploaded parts into the upload directory for this web
-     * application.
-     * @param parts The Collection of Part objects to write.
-     * @return The path to the new file or null.
-     */
-    public String writeUploadedFiles(Collection<Part> parts) {
-        String path = null;
-        for (Part part : parts) {
-            try {
-                Timestamp stamp = new Timestamp(System.currentTimeMillis());
+                // Verify that files were successfully written to disk
+                if (path != null) {
 
-                String analysisPath = uploadPath + "/";
-                initializeUploadPath(analysisPath);
+                    // Parse CSV to JSON
+                    CodingCompCsvUtil parser = new CodingCompCsvUtil();
+                    Map<List<String>, String> values = parser.readCsvFileFileWithoutPojo(path);
 
-                path = analysisPath + stamp.toString() + ".csv";
-                part.write(path);
-            } catch (IOException exception) {
-                logger.error("Error writing uploaded file!", exception);
-                exception.printStackTrace();
-            } catch (Exception exception) {
-                logger.error("Unknown exception!", exception);
-                exception.printStackTrace();
+                    // Retrieve columns list and parsed JSON, expecting only 1 entry in the map
+                    Map.Entry<List<String>, String> entry = values.entrySet().iterator().next();
+                    List<String> columns = entry.getKey();
+                    String rawJson = entry.getValue();
+
+                    // Store column and JSON data
+                    HttpSession session = request.getSession();
+                    session.setAttribute("columns", columns);
+                    session.setAttribute("json", rawJson);
+
+                    // Escape loop since only 1 CSV file is allowed at a time
+                    return true;
+                } else {
+                    logger.error("Failed to write uploaded files to disk!");
+                }
             }
-        }
-        return path;
-    }
-
-    /**
-     * Deletes the file at a given path if it exists.
-     * @param path The String path of the directory to create.
-     * @return Whether or not the file was able to be deleted.
-     */
-    public boolean deleteFile(String path) {
-        File filePath = new File(path);
-        if (filePath.exists()) {
-            try {
-                return filePath.delete();
-            } catch (SecurityException exception) {
-                logger.error("Unable to delete file due to lack of permissions.", exception);
-            } catch (Exception exception) {
-                String message = String.format("Unknown error occurred while removing uploaded file %s.", path);
-                logger.error(message, exception);
-            }
+        } catch (IOException exception) {
+            logger.error("IO exception occurred while processing uploaded file parts.", exception);
+        } catch (Exception exception) {
+            logger.error("Unknown exception occurred while processing uploaded file parts.", exception);
         }
         return false;
     }
