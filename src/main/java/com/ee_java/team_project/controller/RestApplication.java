9d7c7b0b7b5043739971831ed30f363eb330e67c
ClassyElm <26029162+ClassyElm@users.noreply.github.com>
2021-04-11 00:05:03 -0500

    Add MultiPartFeature class for registration of MultiPart form submissions

--- src/main/java/com/ee_java/team_project/controller/RestApplication.java
+++ src/main/java/com/ee_java/team_project/controller/RestApplication.java
@@ -1,8 +1,11 @@
 package com.ee_java.team_project.controller;
 
+import org.glassfish.jersey.media.multipart.MultiPart;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
@@ -17,8 +20,8 @@ public class RestApplication extends Application {
     @Override
     public Set<Class<?>> getClasses() {
         HashSet classes = new HashSet<Class<?>>();
-        classes.add(MultiPartFeature.class);
         classes.add(JSONQueryService.class);
+        classes.add(MultiPartFeature.class);
         return classes;
     }
 }
